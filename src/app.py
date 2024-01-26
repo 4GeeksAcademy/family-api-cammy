86d1e8648ca31800efc4c3b4753642079fcdad1b
Cammy Hale <cammyhale21@gmail.com>
2024-01-26 01:56:18 +0000

    First commit

--- src/app.py
+++ src/app.py
@@ -31,7 +31,6 @@ def all_members():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
         "family": members
     }
 
